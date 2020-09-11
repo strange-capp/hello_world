9f2f205b1380316c696ff2704cbd64da7354dca0
strange-capp <iknowwearewrong@gmail.com>
2020-09-11 22:03:40 +0300

    add views

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<string:name>')
+def hello(name):
+    return render_template('hello.html', name=name)
